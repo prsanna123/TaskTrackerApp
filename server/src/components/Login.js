faef5a158af3d83a3301c64a9b942cb279b242f0
LakshmiPrasanna <94688984+prsanna123@users.noreply.github.com>
2023-11-28 11:49:35 +0530

    Update Login.js

--- server/src/components/Login.js
+++ server/src/components/Login.js
@@ -9,7 +9,7 @@ const Login = (props) => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch("https://task-tracker-app-6go3.vercel.app/api/auth/login", {
+        const response = await fetch("https://task-tracker-app-six.vercel.app/api/auth/login", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
